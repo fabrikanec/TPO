8515908770c3374e867aa4738085235f5c3b74fb
cezar <cezar@localhost.localdomain>
2017-03-28 16:40:38 +0300

    try to set scale

--- Lab2/src/main/java/function/trigonometric/Cos.java
+++ Lab2/src/main/java/function/trigonometric/Cos.java
@@ -2,6 +2,10 @@ package function.trigonometric;
 
 import function.AbstractFunction;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 import static java.lang.Math.*;
 
 public class Cos extends AbstractFunction {
@@ -26,12 +30,16 @@ public class Cos extends AbstractFunction {
 
         sin.setAccuracy(accuracy);
 
-        double unsignedCos = sqrt(1 - pow(sin.calc(arg), 2));
+        //double unsignedCos = (sqrt(1 - pow(sin.calc(arg), 2)));
+        double unsignedCos = BigDecimal.ONE.subtract(BigDecimal.valueOf(sin.calc(arg))
+                             .pow(2, MathContext.DECIMAL128), MathContext.DECIMAL128)
+                             .pow(-2, MathContext.DECIMAL128).doubleValue();
+
         double tmpA = abs(abs(arg) > PI*2 ? arg % PI*2 : arg);
 
         if(tmpA >= 0 && tmpA <= PI/2 || tmpA >= 3*PI/2 && tmpA <= 2*PI)
             return unsignedCos;
         else
-            return  -unsignedCos;
+            return -unsignedCos;
     }
 }
